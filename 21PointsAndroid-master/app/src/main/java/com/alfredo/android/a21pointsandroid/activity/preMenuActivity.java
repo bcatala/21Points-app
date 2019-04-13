d60b160c91571149a1a6ae6773d9b91c8eeb772d
AlvaroCastillejo <alvaro.castillejo@students.salle.url.edu>
2019-04-13 21:45:04 +0200

    #3 Fix. Fixed the pre-Menu.
    Before this commit pre-menu didn't show. Now it does. Non-functional yet.

--- 21PointsAndroid-master/app/src/main/java/com/alfredo/android/a21pointsandroid/activity/preMenuActivity.java
+++ 21PointsAndroid-master/app/src/main/java/com/alfredo/android/a21pointsandroid/activity/preMenuActivity.java
@@ -1,20 +1,23 @@
 package com.alfredo.android.a21pointsandroid.activity;
 
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
 import com.alfredo.android.a21pointsandroid.R;
 import com.alfredo.android.a21pointsandroid.restapi.RestAPIManager;
 
-public class preMenuActivity {
+public class preMenuActivity extends AppCompatActivity {
     private Button GotoMenu;
 
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_premenu);
 
-
-
-        GotoMenu.setOnClickListener(new View.OnClickListener() {
+        Button mGotoMenu = (Button) findViewById(R.id.GotoMenu);
+        mGotoMenu.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                // setContentView(R.layout.activity_main);
