153e1413f7da98ff69ba39008d9ae3116b7a042c
AlvaroCastillejo <alvaro.castillejo@students.salle.url.edu>
2019-04-14 12:59:18 +0200

    #12 AddPoints UI done.
    Created the UI.xml and the activity for the AddPoints functionality. Added to AndroidManifest.xml. Not working yet. Accessible.

---
+++ 21PointsAndroid-master/app/src/main/java/com/alfredo/android/a21pointsandroid/activity/AddPoints.java
@@ -0,0 +1,17 @@
+package com.alfredo.android.a21pointsandroid.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import com.alfredo.android.a21pointsandroid.R;
+
+public class AddPoints extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_addpoints);
+
+    }
+}
